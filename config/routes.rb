752e10dd45477c3138cf142f0c8da9ae474c15c4
daemien <dimmaaah@live.nl>
2017-02-25 09:30:00 +0100

    added style to projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 	  get 'projects/index'
 	  get 'projects/rubyrubyruby'
 	  get 'projects/healthyworkers'
-	end
 
 	  root 'home#index'
+	end
+	root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
